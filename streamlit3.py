b63b817f8f2b2899091bd0d2aa57bb9f56588ad1
invincible361 <161327275+invincible361@users.noreply.github.com>
2025-07-18 08:30:51 +0530

    first streamlit

--- streamlit3.py
+++ streamlit3.py
@@ -2,3 +2,12 @@ import streamlit as st
 st.header("welcome to streamlit")
 name=st.text_input("Enter your name")
 st.button("Submit", on_click=lambda: st.write(f"Hello, {name}! Welcome to Streamlit!"))
+
+import pandas as pd
+file=st.file_uploader("upload csv file",type="csv")
+if file:
+    df=pd.read_csv(file)
+    st.write(df)
+    st.line_chart(df)
+    st.bar_chart(df)
+    st.area_chart(df)
